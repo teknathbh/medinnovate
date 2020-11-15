8320631434a69e76881b6e0295b053808b920c26
Aashish Bhandari <64518389+awebisam@users.noreply.github.com>
2020-11-15 09:34:14 +0545

    Update settings.py

--- med_innovate/settings.py
+++ med_innovate/settings.py
@@ -143,4 +143,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hello.medinnovate@gmail.com'
-EMAIL_HOST_PASSWORD = '123ABC098'
+EMAIL_HOST_PASSWORD = 'SECRET'
