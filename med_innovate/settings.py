3bdd4472d4f65d40ab9b75221478ebf7dc4af14e
Aashish Bhandari <64518389+awebisam@users.noreply.github.com>
2020-11-20 11:06:38 +0545

    Update settings.py

--- med_innovate/settings.py
+++ med_innovate/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7+ne=s8f5m8vd%y=cw-n0-e^gd9j&wyhs-oo*kzlxg4ugdsm$o'
+SECRET_KEY = '//\\//\\//\\//\\//\\'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
